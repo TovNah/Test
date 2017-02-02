5665732c486b81526f8b13601afdc0a2892193ad
bolotov-aa <bolotov-aa@dartit.ru>
2017-02-02 16:16:53 +0500

    commit

--- Windows.py
+++ Windows.py
@@ -1,8 +1,8 @@
 import sys
 from PyQt5.QtWidgets import (QWidget, QToolTip,
-    QPushButton, QApplication, QHBoxLayout)
+    QPushButton, QApplication, QHBoxLayout, QSystemTrayIcon, QLineEdit)
 from PyQt5.QtGui import QFont, QIcon
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QCoreApplication, Qt
 import subprocess
 import SystemTrayIcon
 
@@ -21,27 +21,52 @@ class Windows(QWidget):
         self.setToolTip('This is a <b>QWidget</b> widget')
         self.tray_icon = SystemTrayIcon.SystemTrayIcon(QIcon('idea.png'), self)
         self.tray_icon.show()
+        self.setStylesheet("border-radius: 10px")
+        # self.autoFillBackground(True)
+        
         btn = QPushButton('Кнопка', self)
         btn.setToolTip('This is a <b>QPushButton</b> widget')
         btn.resize(btn.sizeHint())
-        btn.move(200, 50)
+        btn.move(100, 10)
         btn.clicked.connect(Windows.notif)
         
         exit = QPushButton('Выход', self)
         exit.resize(btn.sizeHint())
-        exit.move(50, 50)
+        exit.move(10, 10)
         exit.clicked.connect(QCoreApplication.instance().quit)
+
+        textSearch = QLineEdit(self)
+        textSearch.move(200, 10)
         
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(btn)
         hbox.addWidget(exit)
+        hbox.addWidget(textSearch)
+
+       
 
         
 
-        self.setGeometry(300, 300, 300, 200)
+        self.setGeometry(1500, 400, 700, 50)
         self.setWindowTitle('Tooltips')
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+
+        # self.tray_icon.showMessage(
+        #         "Tray Program",
+        #         "Application was minimized to Tray",
+        #         QSystemTrayIcon.Information,
+        #         2000
+        #     )
         # self.show()
 
     def notif(self):
         subprocess.Popen(['notify-send', 'Hello World '])
+
+        # self.tray_icon.showMessage(
+        #         "Tray Program",
+        #         "Application was minimized to Tray",
+        #         QSystemTrayIcon.Information,
+        #         2000
+        #     )
